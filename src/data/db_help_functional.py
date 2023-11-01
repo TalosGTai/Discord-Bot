ddb5b7404b6c0f6edf153b45dc1c0c2f8fccc402
Olga Matveeva <matveevaolga2882@gmail.com>
2023-11-01 19:45:24 +0300

    edited db_help_functional.py module

--- src/data/db_help_functional.py
+++ src/data/db_help_functional.py
@@ -9,14 +9,15 @@ def create_user(user):
     '''Создание пользователя в БД'''
     
     data_base = db.DB()
-    users, stats, duel = divide_values(user)
+    users, stats, duel, lucky = divide_values(user)
     new_id = data_base.get_last_user_id() + 1
 
     print(users, new_id)
 
-    data_base.insert_stats(stats, new_id)
+    data_base.insert_stats([new_id] + stats, new_id)
     data_base.insert_users(users, new_id)
-    data_base.insert_duel(duel, new_id)
+    data_base.insert_duel([new_id] + duel, new_id)
+    data_base.insert_table_lucky_number([new_id] + lucky)
 
 
 def divide_values(values: list) -> tuple:
@@ -27,8 +28,9 @@ def divide_values(values: list) -> tuple:
              values.count_done_help, values.count_projects,
              values.bonus_rate, values.rate]
     duels = [values.duel_all_games, values.duel_win_games]
+    lucky = [0, 0]
 
-    return (users, stats, duels)
+    return users, stats, duels, lucky
 
 
 def get_number_random_question(table: str) -> int | None:
